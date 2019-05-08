9ea0ba28cbd20e7731345d8ebe539ec03206889f
Jorge <jorge.plaza@usach.cl>
2019-05-08 08:33:24 -0400

    Primer commit

---
+++ lab3.py
@@ -0,0 +1,13 @@
+import numpy as np
+import scipy
+import matplotlib
+import scipy.io.wavfile as wav
+import matplotlib.pyplot as plotFunction
+from scipy.fftpack import fft as fourier
+from scipy.fftpack import ifft as inverseFourier 
+from scipy.fftpack import fftfreq as frequency
+from scipy import signal
+import pylab
+import math
+
+
